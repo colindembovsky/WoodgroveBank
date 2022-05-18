72f336343436620d6b514e91990c590d06665c0f
Colin Dembovsky <colindembovsky@gmail.com>
2022-05-18 19:02:34 +0000

    Fixing reg issues

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -8,14 +8,14 @@ echo "$(date)    on-create start" >> "$HOME/status"
 echo "create k3d cluster"
 #alias k=kubectl
 k3d registry create myregistry.localhost --port 12345
-k3d cluster create newcluster --registry-use k3d-myregistry.localhost:12345
+k3d cluster create --registry-use k3d-myregistry.localhost:12345
 
 echo "create namespace"
-#kubectl create namespace woodgrovebank01
-#kubectl config set-context --current --namespace=woodgrovebank01 
+kubectl create namespace woodgrovebank01
+kubectl config set-context --current --namespace=woodgrovebank01 
 
 echo "install nginx ingress"
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/baremetal/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/baremetal/deploy.yaml
 
 echo "install keda"
 helm repo add kedacore https://kedacore.github.io/charts
@@ -26,7 +26,10 @@ echo "install azurerite"
 kubectl apply -f ./k8s/azurerite.yaml
 
 echo "build the containers"
-docker-compose build
+#docker-compose build
+
+echo "push the containers to the local registry"
+#docker-compose push
 
 echo "on-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    on-create complete" >> "$HOME/status"
